ec924f786365a974bb381a049143cada2bd5c976
krivera1015 <krivera1015@gmail.com>
2018-06-27 22:34:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = []
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while(i < facts.length) {
+    facts.push(`${facts[i]}!!!`)
+  }
+return facts;
+}
